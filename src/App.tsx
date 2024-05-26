1afe86da26db8201071b832b65c6640f111c9c4c
Lizeth Vera <lizethvera@Lizeths-MacBook-Pro.local>
2024-05-25 20:11:08 -0700

    Adding the states to handle the new data, and the use effect to fetch it

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import { Container, CssBaseline, Grid } from '@mui/material'
@@ -6,7 +6,9 @@ import Header from './components/Header'
 import Sidebar from './components/Siderbar'
 import Chart from './components/Chart'
 import DataTable from './DataTable/DataTable'
+import { fetchData } from './api'
 
+// Defining interfaces based on data.json.
 interface SalesData {
   weekEnding: string;
   retailSales: number;
@@ -33,7 +35,22 @@ interface ProductData {
 }
 
 
-function App() {
+const App: React.FC = () => {
+
+  //Initiailizing Product data with empty array. 
+  const [data, setData] = useState<ProductData[]>([]);
+  // Set it to true to simulate the data is fectching. This is usefull because it could prevent other components to render without any data. 
+  const [loading, setLoading] = useState(true);
+
+  // UseEffect allows to perform side effects. (e.g.,fetching data in this case). It's created with an empty dependency list [], to run only once after the initial render. In this case we need it only once to fetch the data.
+  useEffect(()=>{
+    fetchData().then((data)=>{
+      setData(data as ProductData[]);
+      setLoading(false);
+    });
+  }, []);
+
+
   return (
     <>
       <CssBaseline />

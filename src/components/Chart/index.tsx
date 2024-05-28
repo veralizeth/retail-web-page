76a01a99682316504c67a8562190e49f38987ce6
Lizeth Vera <lizethvera@Lizeths-MacBook-Pro.local>
2024-05-28 16:06:45 -0700

    Adding title to the chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -27,39 +27,42 @@ const Chart: React.FC<ChartProps> = ({ salesData }) => {
   };
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={montlySums} className="line-chart">
-        <XAxis dataKey="month" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {/* The stroke library does not support classes thats why I let the stroke color explicit here. */}
-        <Line
-          type="monotone"
-          dataKey="retailSales"
-          stroke="#4dabf6"
-          strokeWidth={3}
-          dot={false}
-          name={displayKey('retailSales')}
-        />
-        <Line
-          type="monotone"
-          dataKey="wholesaleSales"
-          stroke="#9ca7c0"
-          strokeWidth={3}
-          dot={false}
-          name={displayKey('wholesaleSales')}
-        />
-        <Line
-          type="monotone"
-          dataKey="retailerMargin"
-          stroke="#008000"
-          strokeWidth={3}
-          dot={false}
-          name={displayKey('retailerMargin')}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <div className="chart-container">
+      <h3 className="chart-title">Retail Sales</h3>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={montlySums} className="line-chart">
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {/* The stroke library does not support classes thats why I let the stroke color explicit here. */}
+          <Line
+            type="monotone"
+            dataKey="retailSales"
+            stroke="#4dabf6"
+            strokeWidth={3}
+            dot={false}
+            name={displayKey('retailSales')}
+          />
+          <Line
+            type="monotone"
+            dataKey="wholesaleSales"
+            stroke="#9ca7c0"
+            strokeWidth={3}
+            dot={false}
+            name={displayKey('wholesaleSales')}
+          />
+          <Line
+            type="monotone"
+            dataKey="retailerMargin"
+            stroke="#008000"
+            strokeWidth={3}
+            dot={false}
+            name={displayKey('retailerMargin')}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
